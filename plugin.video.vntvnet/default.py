9727cef84a061915382790d49f16091538233ef1
chanxuong <chanxuong@gmail.com>
2018-07-16 18:19:29 +1000

    Update other tv

--- plugin.video.vntvnet/default.py
+++ plugin.video.vntvnet/default.py
@@ -53,30 +53,33 @@ def get_channels():
 				item_url = item_link['href']
 				item_name = item_url[item_url.rfind('/')+1:]
 				if item_image is not None:
-					add_link(item_name, 'http://vn.tvnet.gov.vn' + item_link['href'] + '?re=1', item_image['src'])
+					add_vntvnetlink(item_name, 'http://vn.tvnet.gov.vn' + item_link['href'] + '?re=1', item_image['src'])
 				else:
-					add_link(item_name, 'http://vn.tvnet.gov.vn' + item_link['href'], icon)
+					add_vntvnetlink(item_name, 'http://vn.tvnet.gov.vn' + item_link['href'], icon)
+	add_directurl('ANTV', 'http://antvlive.ab5c6921.cdnviet.com/antvmobile/playlist.m3u8', 'http://img.hplus.com.vn/460x260/poster/2018/06/05/524177-anninh.png')
+	add_hpluslink('HTV2', 'http://hplus.com.vn/xem-kenh-htv2-hd-2669.html', 'http://img.hplus.com.vn/460x260/poster/2018/06/05/857149-HTV2.png')
+	add_hpluslink('HTV3', 'http://hplus.com.vn/xem-kenh-htv3-2535.html', 'http://img.hplus.com.vn/460x260/poster/2018/06/05/750643-HTV3.png')
+	add_hpluslink('HTV3', 'http://hplus.com.vn/xem-kenh-htv3-2535.html', 'http://img.hplus.com.vn/460x260/poster/2018/06/05/750643-HTV3.png')
 
-
-def get_channel(url):
-	content = make_request(url)
-	soup = BeautifulSoup(str(content), convertEntities=BeautifulSoup.HTML_ENTITIES)
-	player_div = soup.find('div', {'id' : 'mediaplayer'})
-	if player_div is not None:
-		data_file = player_div['data-file']
-		if data-file is not None:
-			item = xbmcgui.ListItem(path=data_file)
-			xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
-	return ok
-
-
-def add_link(name, href, thumb):
-	u=sys.argv[0]+"?url="+urllib.quote_plus(href.encode('utf8'))+"&mode=1"
+def add_directurl(name, href, thumb):
+	u=sys.argv[0]+"?url="+urllib.quote_plus(href.encode('utf8'))+"&mode=2"
 	liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=thumb)
 	liz.setInfo(type="Video", infoLabels={ "Title": name})
 	liz.setProperty('IsPlayable', 'true')
 	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz)
 
+def add_vntvnetlink(name, href, thumb):
+	u=sys.argv[0]+"?url="+urllib.quote_plus(href.encode('utf8'))+"&mode=1"
+	liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=thumb)
+	liz.setInfo(type="Video", infoLabels={ "Title": name})
+	liz.setProperty('IsPlayable', 'true')
+	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz)
+def add_hpluslink(name, href, thumb):
+	u=sys.argv[0]+"?url="+urllib.quote_plus(href.encode('utf8'))+"&mode=3"
+	liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=thumb)
+	liz.setInfo(type="Video", infoLabels={ "Title": name})
+	liz.setProperty('IsPlayable', 'true')
+	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz)
 
 
 def add_dir(name,url,mode,iconimage,pagenum):
@@ -88,7 +91,7 @@ def add_dir(name,url,mode,iconimage,pagenum):
 	return ok
 	
 
-def resolve_url(url):
+def resolve_vntvneturl(url):
 	ok=True
 	content = make_request(url)
 	
@@ -103,8 +106,45 @@ def resolve_url(url):
 			item = xbmcgui.ListItem(path=stream['url'])
 			xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
 	return ok	
-
+def resolve_directurl(url):
+	print 'resolve direct url'
+	ok=True
+	item = xbmcgui.ListItem(path=url)
+	xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
+	return ok	
+def resolve_hplusurl(url):
+	ok=True
+	content = make_request(url)
 	
+	soup = BeautifulSoup(str(content), convertEntities=BeautifulSoup.HTML_ENTITIES)
+	link_input = soup.find('input', {'id' : 'link-live'})
+	if link_input is not None:
+	
+		video_url = link_input['value']
+		if video_url is not None:
+			headers = {
+				'Accept':'*/*'
+				
+			}
+			form_fields = {
+				"type": 1,
+				"is_mobile": 0,
+				"url": video_url
+			}
+
+			
+			response = urlfetch.fetch(
+				url = 'http://hplus.com.vn/content/getlinkvideo/',
+				method='POST',
+				headers = headers,
+				data=form_fields,
+				follow_redirects = False
+			)
+			
+			print 'test: ', response.content
+			item = xbmcgui.ListItem(path= response.content)
+			xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
+	return ok	
 def get_params():
 	param=[]
 	paramstring=sys.argv[2]
@@ -156,7 +196,10 @@ if mode==None:
 	get_channels()
 
 elif mode==1:
-	resolve_url(url)
-
+	resolve_vntvneturl(url)
+elif mode==2:
+	resolve_directurl(url)
+elif mode==3:
+	resolve_hplusurl(url)
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
