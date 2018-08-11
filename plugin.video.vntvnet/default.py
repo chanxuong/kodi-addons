836025564ca5d5fe0fba9c639209a36090050734
chanxuong <chanxuong@gmail.com>
2018-08-11 18:45:59 +1000

    New version

--- plugin.video.vntvnet/default.py
+++ plugin.video.vntvnet/default.py
@@ -59,17 +59,6 @@ def get_channels():
 	add_directurl('ANTV', 'http://antvlive.ab5c6921.cdnviet.com/antvmobile/playlist.m3u8', 'http://img.hplus.com.vn/460x260/poster/2018/06/05/524177-anninh.png')
 	add_hpluslink('HTV9 HD', 'http://hplus.com.vn/xem-kenh-htv9-hd-2667.html', 'http://img.hplus.com.vn/460x260/poster/2018/07/16/422430-665121-HTV9HD.png')
 	add_hpluslink('HTV7 HD', 'http://hplus.com.vn/xem-kenh-htv7-hd-256.html', 'http://img.hplus.com.vn/460x260/poster/2018/07/16/928755-665121-HTV7HD.png')
-	add_hpluslink('HTV2 HD', 'http://hplus.com.vn/xem-kenh-htv2-hd-2669.html', 'http://img.hplus.com.vn/460x260/poster/2018/06/05/904719-HTV2HD.png')
-	add_hpluslink('HTV3', 'http://hplus.com.vn/xem-kenh-htv3-2535.html', 'http://img.hplus.com.vn/460x260/poster/2018/06/05/750643-HTV3.png')
-	add_hpluslink('HTVC Phim HD', 'http://hplus.com.vn/xem-kenh-htvc-plus-full-hd-1080-2395.html', 'http://img.hplus.com.vn/460x260/poster/2018/06/05/591916-pHIM.png')
-	add_hpluslink('HTVC Plus HD', 'http://hplus.com.vn/xem-kenh-htvc-plus-full-hd-1080-2395.html', 'http://img.hplus.com.vn/460x260/poster/2018/06/05/618172-htvplus.png')
-	add_hpluslink('HTVC Ca Nhac HD', 'http://hplus.com.vn/xem-kenh-htvc-ca-nhac-full-hd-1080-2264.html', 'http://img.hplus.com.vn/460x260/poster/2018/06/05/564807-canhacHD.png')
-	add_hpluslink('Thuan Viet HD', 'http://hplus.com.vn/xem-kenh-htvc-thuan-viet-hd-2398.html', 'http://img.hplus.com.vn/460x260/poster/2018/06/05/136820-ThuanVietHD1.png')
-	add_hpluslink('Thuan Viet', 'http://hplus.com.vn/xem-kenh-htvc-thuan-viet-2396.html', 'http://img.hplus.com.vn/460x260/poster/2018/06/05/504279-ThuanViet.png')
-	add_hpluslink('HTVC Gia Dinh', 'http://hplus.com.vn/xem-kenh-htvc-gia-dinh-full-hd-1080-2394.html', 'http://img.hplus.com.vn/460x260/poster/2018/06/05/866192-Gia-Dinh.png')
-	add_hpluslink('HTVC Du Lich & Cuoc Song', 'http://hplus.com.vn/xem-kenh-htvc-du-lich-va-cuoc-song-full-hd-1080-2328.html', 'http://img.hplus.com.vn/460x260/poster/2018/06/05/305526-dulich.png')
-	add_hpluslink('HTVC Phu Nu', 'http://hplus.com.vn/xem-kenh-htvc-phu-nu-full-hd-1080-2393.html', 'http://img.hplus.com.vn/460x260/poster/2018/06/05/143922-pn.png')
-	add_hpluslink('FBNC', 'http://hplus.com.vn/xem-kenh-fbnc-2184.html', 'http://img.hplus.com.vn/460x260/poster/2018/06/05/906484-FBNC.png')
 	
 
 def add_directurl(name, href, thumb):
